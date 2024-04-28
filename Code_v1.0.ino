6c12ed53dbeada8c68095df35f74aed38f0edd90
Rodrigo de Sousa <rs-rodrigosousa-1998@hotmail.com>
2024-04-28 21:17:38 +0100

    feat: Encoder functionality added
    Signed-off-by: Rodrigo de Sousa <rs-rodrigosousa-1998@hotmail.com>

--- Code_v1.0.ino
+++ Code_v1.0.ino
@@ -1,81 +1,122 @@
-//Pin Definition
-int encoderCRK_DT = 2; // Connected to DT
-int encoderCRK_CLK = 3; // Connected to CLK 
-int encoderCAM_DT = 4; // Connected to DT 
-int encoderCAM_CLK = 5; // Connected to CLK 
-int red_led_1 = 6;
-int red_led_2 = 7;
-int red_led_3 = 8;
-int red_led_4 = 9;
-int green_led_1 = 10;
-int green_led_2 = 11;
-int green_led_3 = 12;
-int green_led_4 = 13; 
+//Macro Definitions
+#define CRANK_ENCODER 0U
+#define CAM_ENCODER   1U
+#define FULL_TURN     30U
+#define ONE_STEP      12U
+#define INIT          0U
+
+//Macro Pin Definitions
+#define ENCODER_CRK_DT   2U
+#define ENCODER_CRK_CLK  3U
+#define ENCODER_CRK_SW   4U
+#define ENCODER_CAM_DT   5U
+#define ENCODER_CAM_CLK  6U
+#define RED_LED_1        8U
+#define RED_LED_2        9U
+#define RED_LED_3        10U
+#define GREEN_LED_1      11U
+#define GREEN_LED_2      12U
+#define GREEN_LED_3      13U
+
  
 //Global Variables
 int aVal;
 boolean bCW;
 int last_CAM_pos;
 int last_CRK_pos;
-int encoderPosCount = 0;
 
 //Main Functions
 void setup() 
 {
-  pinMode (red_led_1   , OUTPUT);
-  pinMode (red_led_2   , OUTPUT);
-  pinMode (red_led_3   , OUTPUT);
-  pinMode (red_led_4   , OUTPUT);
-  pinMode (green_led_1 , OUTPUT);
-  pinMode (green_led_2 , OUTPUT);
-  pinMode (green_led_3 , OUTPUT);
-  pinMode (green_led_4 , OUTPUT);
+  pinMode (RED_LED_1   , OUTPUT);
+  pinMode (RED_LED_2   , OUTPUT);
+  pinMode (RED_LED_3   , OUTPUT);
+  pinMode (GREEN_LED_1 , OUTPUT);
+  pinMode (GREEN_LED_2 , OUTPUT);
+  pinMode (GREEN_LED_3 , OUTPUT);
 
-  pinMode (encoderCRK_CLK , INPUT);
-  pinMode (encoderCRK_DT  , INPUT);
-  pinMode (encoderCAM_CLK , INPUT);
-  pinMode (encoderCAM_DT  , INPUT);
+  pinMode (ENCODER_CRK_DT  , INPUT);
+  pinMode (ENCODER_CRK_CLK , INPUT);
+  pinMode (ENCODER_CRK_SW  , INPUT);  
+  pinMode (ENCODER_CAM_DT  , INPUT);
+  pinMode (ENCODER_CAM_CLK , INPUT);
+
   
-  /* Read Pin A
-  Whatever state it's in will reflect the last position
-  */
-  last_CAM_pos = digitalRead(encoderCAM_CLK);
-  last_CRK_pos = digitalRead(encoderCRK_CLK);
-
-
   Serial.begin (9600);
 }
 
 void loop() 
 {  
-  aVal = digitalRead(encoderCRK_CLK);
-  if (aVal != last_CRK_pos)
-  {                                     // Means the knob is rotating
-                                        // if the knob is rotating, we need to determine direction
-                                        // We do that by reading pin B.
-    if (digitalRead(encoderCRK_DT) != aVal)
-    {                                     // Means pin A Changed first - We're Rotating Clockwise
-      encoderPosCount ++; bCW = true;
-    }  
-    else 
-    {                              // Otherwise B changed first and we're moving CCW bCW = false;
-      encoderPosCount--;
-    }
+  static int last_switch_A = INIT;
+  static int last_switch_B = INIT;
+  static int encoder_pos_count = INIT;
+  static int flip = INIT;
   
-    Serial.print ("Rotated: ");
+  int switch_A = digitalRead(ENCODER_CRK_CLK);
+  int switch_B = digitalRead(ENCODER_CRK_DT);
+  
+  // int button   = digitalRead(ENCODER_CRK_SW);
+  // if (button == 1U)
+  // {
+  //   encoder_pos_count = INIT;
+  // }
 
-    if (bCW)
+
+  if (switch_A != last_switch_A)
+  {    
+    if ((switch_A > switch_B) || flip) 
     {
-      Serial.println ("clockwise");
-    }
+      encoder_pos_count++;
+      
+      Serial.print("Encoder Pos: ");
+      Serial.println(encoder_pos_count);
+
+      flip = ~flip;
+    }  
     else
     {
-      Serial.println("counterclockwise");
+      Serial.print("Encoder Pos: ");
+      Serial.println(encoder_pos_count);
+      encoder_pos_count--;
     }
-
-    Serial.print("Encoder Position: ");
-    Serial.println(encoderPosCount);
-
   }
-  last_CRK_pos = aVal;
+
+  if ((encoder_pos_count == FULL_TURN) || (encoder_pos_count < 0))
+  {
+    encoder_pos_count = INIT;
+  }
+
+  last_switch_A = switch_A;
+  last_switch_B = switch_B;
+
+  // aVal = digitalRead(ENCODER_CRK_CLK);
+  // if (aVal != last_CRK_pos)
+  // {                                     // Means the knob is rotating
+  //                                       // if the knob is rotating, we need to determine direction
+  //                                       // We do that by reading pin B.
+  //   if (digitalRead(ENCODER_CRK_DT) != aVal)
+  //   {                                     // Means pin A Changed first - We're Rotating Clockwise
+  //     encoderPosCount++; 
+  //     bCW = true;
+  //   }  
+  //   else 
+  //   {                              // Otherwise B changed first and we're moving CCW bCW = false;
+  //     encoderPosCount--;
+  //   }
+  
+  //   Serial.print ("Rotated: ");
+
+  //   if (bCW)
+  //   {
+  //     Serial.println ("clockwise");
+  //   }
+  //   else
+  //   {
+  //     Serial.println("counterclockwise");
+  //   }
+
+  //   Serial.print("Encoder Position: ");
+  //   Serial.println(encoderPosCount);
+  // }
+  // last_CRK_pos = aVal;
 }
